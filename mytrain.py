a726776aa6b5d52f6cd2e4e94fec543c69e19392
Itay Ariel <itay@cnvrg.io>
2024-07-02 09:39:45 +0300

    my train file

--- mytrain.py
+++ mytrain.py
@@ -3,9 +3,9 @@ from time import sleep, time
 from datetime import datetime
 
 # Create 'output' folder if it doesn't exist
-if not os.path.exists('output2'):
-    os.makedirs('output2')
-file_name = f"output2/file_50000.txt"
+if not os.path.exists('output'):
+    os.makedirs('output')
+file_name = f"output/file_50000.txt"
 with open(file_name, 'w') as file:
     file.write(f"content")
 print("finished writing")
